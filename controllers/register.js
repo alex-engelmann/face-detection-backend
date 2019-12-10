d97203b4359bf35dfbed81d80d6745593cce7ade
Alex Engelmann <alex.engelmann@gmail.com>
2019-12-10 16:19:42 -0500

    basic regex email validation

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,21 @@
+function validateEmail(email) 
+{
+    //this regex accepts unicode email addresses, 
+    //but is relatively lax (not RFC2822 compliant)
+    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/ ;
+    return re.test(email);
+}
+
 const handleRegister = (req, res, pgDatabase, bcrypt, saltRounds) => {
     //prevents bad user info from registering
     const { email, name, password } = req.body;
     if (!email || !name || !password){
         return res.status(400).json('incorrect form submission');
     }
+    if (!validateEmail(email)) {
+        return res.status(400).json('invalid email address');
+    }
+
     const hash = bcrypt.hashSync(password, saltRounds)
     pgDatabase.transaction(trx => {
         trx.insert({
